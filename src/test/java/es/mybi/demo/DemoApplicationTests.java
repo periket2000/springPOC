a0b32b8df70e10e21b9367608d2020c69f977743
Marco <marco@mybi.es>
2022-05-12 14:46:15 +0200

    Added test.properties for overriding DB configuration. Otherwise, if no physical db is created the initialization fails

--- src/test/java/es/mybi/demo/DemoApplicationTests.java
+++ src/test/java/es/mybi/demo/DemoApplicationTests.java
@@ -5,12 +5,14 @@ import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = DemoApplication.class)
+@TestPropertySource(locations="classpath:test.properties")
 @MemoryDatabaseConfig
 class DemoApplicationTests {
 
